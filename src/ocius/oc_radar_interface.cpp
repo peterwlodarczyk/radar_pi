681bcd71cb2e01425127f718fc5cd4738adc73e8
koopa2@Bluebottle <koopa2@ocius.com.au>
2021-04-08 07:53:17 +0000

    png compression with opti png

--- src/ocius/oc_radar_interface.cpp
+++ src/ocius/oc_radar_interface.cpp
@@ -3,10 +3,10 @@
 #include <chrono>
 #include <fstream>
 #include <string>
-
 #include "oc_utils.h"
 #include "oc_lock.h"
 #include "pi_common.h"
+#include "opngreduc.h"
 
 using namespace std;
 using namespace std::chrono;
@@ -15,6 +15,80 @@ extern std::string g_OciusLiveDir;
 
 static system_clock::time_point next_update = system_clock::now();
 static int oc_count = 0;
+
+void abort_(const char * s, ...)
+{
+	va_list args;
+	va_start(args, s);
+	vfprintf(stderr, s, args);
+	fprintf(stderr, "\n");
+	va_end(args);
+	abort();
+}
+
+
+void write_png_file(char* file_name, png_infop info_ptr, png_bytep * row_pointers)
+{
+	/* create file */
+	FILE *fp = fopen(file_name, "wb");
+	if (!fp)
+		abort_("[write_png_file] File %s could not be opened for writing", file_name);
+
+
+	/* initialize stuff */
+	png_structp png_ptr = png_create_write_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
+
+	if (!png_ptr)
+		abort_("[write_png_file] png_create_write_struct failed");
+
+	//info_ptr = png_create_info_struct(png_ptr);
+	if (!info_ptr)
+		abort_("[write_png_file] png_create_info_struct failed");
+
+	if (setjmp(png_jmpbuf(png_ptr)))
+		abort_("[write_png_file] Error during init_io");
+
+	png_init_io(png_ptr, fp);
+
+
+	/* write header */
+	if (setjmp(png_jmpbuf(png_ptr)))
+		abort_("[write_png_file] Error during writing header");
+
+	int width = png_get_image_width(png_ptr, info_ptr);
+	int height = png_get_image_height(png_ptr, info_ptr);
+	int color_type = png_get_color_type(png_ptr, info_ptr);
+	int bit_depth = png_get_bit_depth(png_ptr, info_ptr);
+
+#if 0
+	png_set_IHDR(png_ptr, info_ptr, width, height,
+			bit_depth, color_type, PNG_INTERLACE_NONE,
+			PNG_COMPRESSION_TYPE_BASE, PNG_FILTER_TYPE_BASE);
+#endif
+	png_write_info(png_ptr, info_ptr);
+
+
+	/* write bytes */
+	if (setjmp(png_jmpbuf(png_ptr)))
+		abort_("[write_png_file] Error during writing bytes");
+
+	png_write_image(png_ptr, row_pointers);
+
+
+	/* end write */
+	if (setjmp(png_jmpbuf(png_ptr)))
+		abort_("[write_png_file] Error during end of write");
+
+	png_write_end(png_ptr, NULL);
+
+	/* cleanup heap allocation */
+  /*
+	for (inty=0; y<height; y++)
+		free(row_pointers[y]);
+	free(row_pointers);
+  */
+	fclose(fp);
+}
 
 void OciusDumpVertexImage(int radar) {
   system_clock::time_point now = system_clock::now();
@@ -33,20 +107,59 @@ void OciusDumpVertexImage(int radar) {
   GLint viewport[4];
   glGetIntegerv(GL_VIEWPORT, viewport);
 
-  int x = viewport[2];
-  int y = viewport[3];
-  int inputSize = x *y * 4;
+  int width = viewport[2];
+  int height = viewport[3];
+  int inputSize = width*height * 4;
   unsigned char *buffer = (unsigned char *)malloc(inputSize);
   memset(buffer, 0, inputSize);
-  glReadPixels(0, 0, x, y, GL_RGBA, GL_UNSIGNED_BYTE, buffer);
+  glReadPixels(0, 0, width, height, GL_RGBA, GL_UNSIGNED_BYTE, buffer);
+  //set the transparent sections based on the 0,0,50 (initial background settings)
+  if (buffer){
+    for (int p = 0; p < width * height * 4; p += 4){ //for each RGBA section
+      if (buffer[p + 0] == 0 && buffer[p + 1] == 0 && buffer[p + 2] == 50){ //default background colour in the config.
+      //todo check if there is a better way to do the above check (compare bits of the whole section?)
+        buffer[p + 3] = 0;
+      }
+    }
+  }
+
+  /*
   int inputSum = 0;
   for (int i = 0; i < inputSize; ++i)
     inputSum += buffer[i];
   OC_TRACE("Vierport:%d,%d,%d,%d.inputSum=%d.\n", viewport[0], viewport[1], viewport[2], viewport[3], inputSum);
+  */
+  
+  //todo replace the wxImage save procedure here with using libpng directly.
+  //todo convert the RGBa buffer created to a 4bitdepth pallete (9 colours - 8 + transparency) when using libpng to save.
+  //use opting to evaluate image compression. 
+  // optipng -full image.png specifies the best output format for the image.
+  //todo reduce the total colours used in the radar to < 8 to reduce file size again.
+  png_bytep * row_pointers = (png_bytep*) malloc(sizeof(png_bytep) * height);
+  for (int i = 0; i < height; i ++){
+    row_pointers[i] = (png_bytep) malloc(width * 4);
+    memcpy(row_pointers[i], buffer + ((height*width*4) - ((i+1)*width*4)), width*4); //mirroring the image as we allocate
+  }
 
+  png_structp png_ptr = png_create_read_struct(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
+	if (!png_ptr)
+		abort_("[read_png_file] png_create_read_struct failed");
+	png_infop info_ptr = png_create_info_struct(png_ptr);
+	if (!info_ptr)
+		abort_("[read_png_file] png_create_info_struct failed");
+
+  //IHDR -> our current settings
+
+  // //png_bytep is a typedef unsigned char png_byte5  
+  png_set_IHDR(png_ptr, info_ptr, width, height, 8, PNG_COLOR_TYPE_RGB_ALPHA, PNG_INTERLACE_NONE , PNG_COMPRESSION_TYPE_DEFAULT, PNG_FILTER_TYPE_DEFAULT);
+  png_set_rows(png_ptr, info_ptr, row_pointers);
+  png_uint_32 reductions = OPNG_REDUCE_RGB_TO_PALETTE | OPNG_REDUCE_8_TO_4_2_1; //reduce the image to palette -> major compression. ~50% reduced file size.
+	png_uint_32 result = opng_reduce_image(png_ptr, info_ptr, reductions);
+
+  /*
   int outputSize = x * y * 3;
-  unsigned char *rgb = (unsigned char *)malloc(outputSize);
-  unsigned char *alpha = (unsigned char *)malloc(x * y);
+  unsigned char *rgb = (unsigned char *)malloc(outputSize); //this wasn't being free'd... memory leaks?
+  unsigned char *alpha = (unsigned char *)malloc(x * y); //this wasn't being free'd... memory leaks?
   if (buffer && rgb) {
     for (int p = 0; p < x * y; p++) {
       rgb[3 * p + 0] = buffer[4 * p + 0];
@@ -60,49 +173,42 @@ void OciusDumpVertexImage(int radar) {
       }
     }
   }
-  free(buffer);
+  */
+  //turn buffer into png_ptr & info_ptr -> this might be too expensive, possibly make these global and just modify them over and over?
+  /*
   wxImage image(x, y);
   image.SetData(rgb);
   image.SetAlpha(alpha);
-
-  image = image.Mirror(false);
-  //image.SetOption("quality", 100); //only used for jpg.
-  image.SetOption(wxIMAGE_OPTION_PNG_BITDEPTH, 4); //Should reduce size a bunch. Changes colours.
-  image.SetOption(wxIMAGE_OPTION_PNG_COMPRESSION_LEVEL, 9); //max compression level.
+  image = image.Mirror(false);  
+  image.SetOption(wxIMAGE_OPTION_PNG_BITDEPTH, 8); //minimum bit depth. //todo change to palette if possible.
+  //the below options are determined by optipng
+  image.SetOption(wxIMAGE_OPTION_PNG_COMPRESSION_LEVEL, 9);
+  image.SetOption(wxIMAGE_OPTION_PNG_COMPRESSION_MEM_LEVEL, 8); 
+  image.ConvertAlphaToMask(wxIMAGE_ALPHA_THRESHOLD); 
+  image.SetOption(wxIMAGE_OPTION_PNG_COMPRESSION_STRATEGY, 1);
+  */
   string filename = g_OciusLiveDir + '/' + name + "-capture.png";
   {
     FileLock f(filename.c_str());
-    CreateFileWithPermissions(filename.c_str(), 0666);
-    if (image.SaveFile(filename.c_str(), wxBITMAP_TYPE_PNG))
-      OC_DEBUG("Wrote file %s.", filename.c_str());
-    else
-      OC_DEBUG("Failed to write file %s.", filename.c_str());
+    if (f.locked())
+    {
+      //CreateFileWithPermissions(filename.c_str(), 0666);
+      write_png_file((char*) filename.c_str(), info_ptr, row_pointers);
+      /*
+      if (image.SaveFile(filename.c_str(), wxBITMAP_TYPE_PNG))
+        OC_DEBUG("Wrote file %s.", filename.c_str());
+      else
+        OC_DEBUG("Failed to write file %s.", filename.c_str());
+      */
+    }
   }
-  
-  // Write it to a file with a comment of the timestamp
-  //wxMemoryOutputStream writeBuffer;
-  //if (image.SaveFile(writeBuffer, wxBITMAP_TYPE_JPEG) && writeBuffer.GetOutputStreamBuffer()->GetBufferSize() > 0) {
-  //  string timestamp = MakeLocalTimeStamp();
-  //  OC_TRACE("ImageSize=%d.time=%s", writeBuffer.GetOutputStreamBuffer()->GetBufferSize(), timestamp.c_str());
-  //  vector<uint8_t> uncommented;
-  //  uncommented.assign(reinterpret_cast<const uint8_t*>(writeBuffer.GetOutputStreamBuffer()->GetBufferStart()), reinterpret_cast<const uint8_t*>(writeBuffer.GetOutputStreamBuffer()->GetBufferEnd()));
-  //  auto commented = JpegAppendComment(uncommented, timestamp, "radar0");
 
-  //  OC_TRACE("%s=%d\n", filename.c_str(), commented.size());
-  //  ofstream imageFile;
-  //  CreateFileWithPermissions(filename.c_str(), 0666);
-  //  imageFile.open(filename.c_str(), ios::out | ios::binary);
-  //  // TODO: Do file locking
-  //  imageFile.write(reinterpret_cast<const char *>(&commented.front()), commented.size());
-  //  if (imageFile)
-  //    OC_DEBUG("Wrote %d bytes to file %s:%s.", commented.size(), filename.c_str(), timestamp.c_str());
-  //  else
-  //    OC_DEBUG("Failed to write file %s.", filename.c_str());
-  //  imageFile.close();
-  //}
-  //else {
-  //  OC_DEBUG("ERROR: Faled to write GL image to buffer.");
-  //}
-  
+  free(buffer);
+  for (int i = 0; i < height; i ++){
+    if (row_pointers[i]){
+      free(row_pointers[i]);
+    }
+  }
+  free(row_pointers);
   OC_TRACE("[OciusDumpVertexImage]<<");
 }
