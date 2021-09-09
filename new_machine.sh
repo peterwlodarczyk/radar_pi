b063a6be35f74f9783003d59780bcfeabb37c7b3
bluey2 bluebottle <bluey2@ocius>
2021-09-09 00:28:04 +0000

    Added new_machine.sh

---
+++ new_machine.sh
@@ -0,0 +1,2 @@
+sudo apt-get install build-essential cmake gettext git-core gpsd gpsd-clients libgps-dev wx-common libglu1-mesa-dev libgtk2.0-dev wx3.0-headers libbz2-dev libtinyxml-dev libportaudio2 portaudio19-dev libcurl4-openssl-dev libexpat1-dev libcairo2-dev libarchive-dev liblzma-dev libexif-dev libelf-dev libsqlite3-dev
+
