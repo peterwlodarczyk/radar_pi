dbed8e43380850d4151999edaf1edd89ce2efaef
Peter Wlodarczyk <peter@ocius.com.au>
2021-09-26 17:39:24 +1000

    Added guardzone overlay.

--- src/exe/radar_lib.h
+++ src/exe/radar_lib.h
@@ -65,6 +65,7 @@ extern "C" DECL_IMPEXP uint32_t radar_get_image_count(uint8_t radar); // the num
 extern "C" DECL_IMPEXP bool radar_set_guardzone_state(uint8_t radar, uint8_t zone, int state);
 extern "C" DECL_IMPEXP bool radar_set_guardzone_arpa(uint8_t radar, uint8_t zone, int state);
 extern "C" DECL_IMPEXP bool radar_set_guardzone_type(uint8_t radar, uint8_t zone, int type);
+extern "C" DECL_IMPEXP bool radar_set_guardzone_overlay(uint8_t radar, uint8_t zone, int show);
 extern "C" DECL_IMPEXP bool radar_set_guardzone_dimensions(uint8_t radar, uint8_t zone, const int* dims);
 extern "C" DECL_IMPEXP bool radar_marpa_acquire(uint8_t radar, float lat, float lon);
 extern "C" DECL_IMPEXP bool radar_marpa_delete(uint8_t radar, float lat, float lon);
